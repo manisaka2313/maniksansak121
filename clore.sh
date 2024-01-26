824c3faf7cfd4afe21f245f8f3c3990c50e0b319
manisaka2313 <155189546+manisaka2313@users.noreply.github.com>
2024-01-26 16:47:53 +0700

    342342

---
+++ clore.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# replace 0000 with your address
+
+# mine to rplant
+./bzminer -a clore -w 0000 -p stratum+ssl://stratum-eu.rplant.xyz:17083 --nc 1
+
+read -p "Press [Enter] key to start continue..."
--- mine_kas.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-POOL=pool.woolypooly.com:3113
-WALLET=kaspa:qqgsckw6auwxpne56c9jr7w645ef6tgkvaydadhlegwnaz7uv8haje9vdkzvq
-
-./lolMiner --algo KASPA --pool $POOL --user $WALLET $@
-
